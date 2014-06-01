94fe784016cd057df41326d9f96749b6b2767356
Damien Corpataux <d@mien.ch>
2014-05-31 21:38:21 -0400

    OSM scrapper code cleanup

--- data/scrapper/osm.py
+++ data/scrapper/osm.py
@@ -1,49 +1,48 @@
-import os, sys, json
-import subprocess
+"""
+Retrieves Switzerland bus stops from OSM extract (geofabrik).
+"""
+
+import sys, subprocess
 import xml.etree.cElementTree as tree
 
-# TODO: from http://zippy1978.tumblr.com/post/21800273257/extracting-data-from-openstreetmap-to-build-nice
+# Alternative filtering, using osmosis
+#     from http://zippy1978.tumblr.com/post/21800273257/extracting-data-from-openstreetmap-to-build-nice
 # sudo apt-get install osmosis
 # wget http://download.geofabrik.de/europe/switzerland-latest.osm.bz2
 # bunzip2 switzerland-latest.osm.bz2
 # osmosis --read-xml switzerland-latest.osm --tf accept-nodes highway=bus_stop --write-xml file=stops.osm
 
-filename = '/tmp/ch.osm'
-
 def stops_bs4():
     import bs4
     with open(filename) as file:
+        filename = '/tmp/ch.osm'
         soup = bs4.BeautifulSoup(file.read())
 
 def stops_etree():
-    ##cmd = 'wget -O - http://download.geofabrik.de/europe/switzerland-latest.osm.bz2 2>/dev/null | bunzip2 - > %s' % filename
-    cmd = 'wget -O - http://download.geofabrik.de/europe/switzerland-latest.osm.bz2 | bunzip2 - > %s' % filename
-    p = subprocess.call(cmd, shell=True)
-    with open(filename) as file:
-        c = 0
-        stops = []
-        # http://effbot.org/zone/element-iterparse.htm#incremental-parsing
-        context = iter(tree.iterparse(file, events=("start", "end")))
-        event, root = context.next()
-        for event, elem in context:#tree.iterparse(file): #p.stdout ? it has a .read() too
-            c += 1
-            sys.stdout.write('Processing: node %s %s %s\r' % (c, elem.tag, ' '*5))
-            if event == 'end':
-                if elem.tag == "node":
-                    if [tag for tag in elem.findall('tag')
-                        if tag.get('k') == 'highway'
-                        and tag.get('v') == 'bus_stop']:
-                        stop = dict(
-                            {tag.get('k'):tag.get('v') for tag in elem.findall('tag')},
-                            **{'lon': elem.get('lon'),
-                               'lat': elem.get('lat'),
-                               'uid': elem.get('uid'),
-                               'version': elem.get('version')})
-                        print '\n%s'%stop
-                        yield stop
-                    elem.clear()
-                root.clear()
-    os.remove(filename)
-
+    cmd = 'wget -O - http://download.geofabrik.de/europe/switzerland-latest.osm.bz2 | bunzip2 -'
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    count = 0
+    # http://effbot.org/zone/element-iterparse.htm#incremental-parsing
+    context = iter(tree.iterparse(process.stdout, events=("start", "end")))
+    event, root = context.next()
+    for event, elem in context:
+        count += 1
+        sys.stdout.write('Processing: node %s %s %s\r'
+                         % (count, elem.tag, ' '*5))
+        if event == 'end':
+            if elem.tag == "node":
+                if [tag for tag in elem.findall('tag')
+                    if tag.get('k') == 'highway'
+                    and tag.get('v') == 'bus_stop']:
+                    stop = dict(
+                        {tag.get('k'):tag.get('v') for tag in elem.findall('tag')},
+                        **{'lon': elem.get('lon'),
+                           'lat': elem.get('lat'),
+                           'uid': elem.get('uid'),
+                           'version': elem.get('version')})
+                    print '\n%s'%stop
+                    yield stop
+                elem.clear()
+            root.clear()
 
 stops = stops_etree
